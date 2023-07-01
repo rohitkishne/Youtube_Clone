15a342a64b5e42f7156b4dc2531ed6111f5cbab7
Rohit kishne <rohitkishne3@gmail.com>
2023-07-01 22:38:06 +0530

    Youtube clone

---
+++ scripts/index.js
@@ -0,0 +1,64 @@
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
+const API_KEY = "AIzaSyCkJL7aWZ0XqGopK1r-X0XX8n7sA2L5uC4";
+
+const allVidoesContainer = document.getElementById("yt-uploaded-videos");
+
+async function getVideos(searchString) {
+    const url = `${BASE_URL}/search?key=${API_KEY}&q=${searchString}&part=snippet&type=video&maxResults=20`;
+    try {
+        const response = await fetch(url,{
+            method:'get'
+        });
+        const data = await response.json();
+    
+        const videos = data.items;
+        renderVideos(videos);
+    } catch (error) {
+        allVidoesContainer.innerHTML = '';
+        allVidoesContainer.innerHTML += 
+        `<div class="error">
+            You Have reached a Today's Limit of Fetching API's
+        </div>`
+
+    }
+}
+
+const searchVideo = document.getElementById("search");
+searchVideo.addEventListener("keyup" , (event) => {
+    const keyword = event.target.value;
+    getVideos(keyword)
+})
+
+
+function renderVideos(videos){
+    allVidoesContainer.innerHTML = '';
+    videos.forEach(video => {
+        const thumbnailUrl = video.snippet.thumbnails.high.url;
+        allVidoesContainer.innerHTML += `
+                                    <div class="video-info" onclick="openVideoDetails('${video.id}')">
+                                        <div class="video-card">
+                                            <div class="thumbnail">
+                                                <img src="${thumbnailUrl}" alt="logo">
+                                            </div>
+                                            <div class="owner-title">
+                                                <span class="owner-photo"><img src="./Assets/images/Profile.png" alt="owner pic"></span>
+                                                <span class="video-desc">${video.snippet.title}</span>
+                                            </div>
+                                            <div class="owner-name">
+                                                <span>${video.snippet.channelTitle}</span>
+                                            </div>
+                                            <div class="views-upload-date">
+                                                <span>15k views </span>
+                                                <span>1 week ago</span>
+                                            </div>
+                                        </div> 
+                                    </div>`
+    });
+}
+
+function openVideoDetails(videoId) {
+    localStorage.setItem("videoId", videoId);
+    window.open("/videoDetails.html");
+}
+
+getVideos("")
---
+++ scripts/videoDetail.js
@@ -0,0 +1,128 @@
+const BASE_URL_1 = "https://www.googleapis.com/youtube/v3";
+const API_KEY_1 = "AIzaSyCkJL7aWZ0XqGopK1r-X0XX8n7sA2L5uC4";
+
+const videoContainer = document.getElementById("yt-videoId");
+
+const videoId = localStorage.getItem("videoId");
+
+const commentsContainer = document.getElementById("comment");
+
+videoContainer.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`
+
+
+async function getVideoDetail(videoId) {
+    const url = `${BASE_URL_1}/videos?part=snippet,contentDetails,statistics&id=${videoId}&key=${API_KEY_1}`
+
+    try {
+        const response = await fetch(url, {
+            method:"get"
+        });
+        const data = await response.json();
+         
+        renderVideoDetails(data);
+
+    } catch (error) {
+        const videoInfo = document.getElementById("videoInfo")
+        videoInfo.innerHTML ='';
+        videoInfo.innerHTML += `
+                        <div>
+                            Sorry No Data Found
+                        </div>
+                        `
+    }
+    
+}
+
+const videoDesc = document.getElementById("video-desc");
+
+function renderVideoDetails(video) {
+    videoDesc.innerHTML = `
+                        <div class="video-title">
+                            ${video.snippet.title}
+                        </div>
+                        <div class="likes-views">
+                            <div class="like-left">
+                                <span>${video.statistics.viewCount} views</span>
+                                <span>Oct 8, 2021</span>
+                            </div>
+                            <div class="like-right">
+                                <img src="./Assets/images/videoDetails/video-share/Top-Level.png" alt="">
+                            </div>
+                        </div>
+
+                    `
+}
+
+
+async function getComments() {
+    const url = `${BASE_URL_1}/commentThreads?key=${API_KEY_1}&part=snippet&videoId=${videoId}&maxResults=25&order=time`
+    try {
+        const response = await fetch(url, {
+            method:"get"
+        });
+        const data = await response.json();
+
+        const comments = data.items;
+
+        renderComments(comments);
+
+    } catch (error) {
+        commentsContainer.innerHTML = '';
+        commentsContainer.innerHTML +=`
+                            <div class="comment-error">
+                                Comments are not Found!
+                            </div>
+                                `
+    }
+}
+
+function renderComments(comments) {
+    commentsContainer.innerHTML = '';
+    comments.forEach(comment => {
+        commentsContainer.innerHTML += `
+                        <div class="comment-person">
+                            <img src="./Assets/images/Header/Profile.png" alt="">
+                        </div>
+                        <div class="comment-info">
+                            <div class="person-name">
+                                <span class="name">
+                                    James Gouse
+                                </span>
+                                <span class="when">
+                                    8 hours ago
+                                </span>
+                            </div>
+                            <div class="comment-type">
+                                <span>
+                                    ${comment.snippet.topLevelComment.snippet.textDisplay}
+                                </span>
+                            </div>
+                            <div class="reply">
+                                <div class="comment-like">
+                                    <span>
+                                        <img src="./Assets/images/videoDetails/like-dislike/like.png" alt="">
+                                    </span>
+                                    <span class="likeno">
+                                        3
+                                    </span>
+                                </div>
+                                <div class="comment-dislike">
+                                    <span>
+                                        <img src="./Assets/images/videoDetails/like-dislike/dislike.png" alt="">
+                                    </span>
+                                </div>
+                                <button class="reply-btn" onclick="seeReply()">
+                                    REPLY
+                                </button>
+                            </div>
+                        </div>`
+    });
+}
+
+
+async function seeReply() {
+    
+}
+
+// getVideoDetail(videoId);
+// getComments();
